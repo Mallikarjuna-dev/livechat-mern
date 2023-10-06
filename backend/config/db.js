2d903b59494800bc18f867aa552baea00f753d8b
Mallikarjuna Annigeri <76250603+Mallikarjuna-dev@users.noreply.github.com>
2023-10-06 17:47:28 +0530

    connection to mongoDB

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`mongodb connected :${db.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.log(`mongodb connection error: ${error}`.red.bold);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-
+const colors = require("colors");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
@@ -9,4 +12,4 @@ app.get("/", (req, res) => {
   res.send("api running");
 });
 
-app.listen(port, console.log(`server listening on port: ${port}`));
+app.listen(port, console.log(`server listening on port: ${port}`.yellow.bold));
