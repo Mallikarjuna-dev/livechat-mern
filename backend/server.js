9d6fae22a5c6dd7cc81e71fb7c30ef2391551eea
Mallikarjuna Annigeri <76250603+Mallikarjuna-dev@users.noreply.github.com>
2024-04-22 10:16:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ const server = app.listen(
 );
 
 const io = require("socket.io")(server, {
-  pingTimeout: 20000,
+  pingTimeout: 2000,
   cors: {
     origin: "http://localhost:3000",
   },
@@ -74,15 +74,19 @@ io.on("connection", (socket) => {
   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
   socket.on("new message", (newMessageRecieved) => {
-    var chat = newMessageRecieved.chat;
+    try {
+      var chat = newMessageRecieved.chat;
 
-    if (!chat.users) return consol.log("chat.users not defined");
+      if (!chat.users) return console.log("chat.users not defined");
 
-    chat.users.forEach((user) => {
-      if (user._id == newMessageRecieved.sender._id) return;
+      chat.users.forEach((user) => {
+        if (user._id == newMessageRecieved.sender._id) return;
 
-      socket.in(user._id).emit("message recieved", newMessageRecieved);
-    });
+        socket.in(user._id).emit("message recieved", newMessageRecieved);
+      });
+    } catch (error) {
+      console.error("Error handling new message:", error);
+    }
   });
 
   // socket.off("setup", () => {
