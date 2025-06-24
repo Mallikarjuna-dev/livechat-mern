b752b4b3a9b48a560c7749993a2977aa3b69451f
Mallikarjuna-dev <76250603+Mallikarjuna-dev@users.noreply.github.com>
2025-06-25 00:59:00 +0530

    socket.io updates with real time tyoing indicator

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,6 +2,9 @@ import { io } from "socket.io-client";
 
 const ENDPOINT = "http://localhost:5000";
 
-const socket = io(ENDPOINT);
+// const socket = io(ENDPOINT);
+const socket = io(window.location.origin)
 
 export default socket;
+
+
